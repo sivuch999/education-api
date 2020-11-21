e03f1e92c0930f67ee261e2c69f619c537180bec
sivuch999 <slowsoup@hotmail.com>
2020-11-21 19:43:02 +0700

    add condition

--- functions/app/controllers/parcels/getAll.js
+++ functions/app/controllers/parcels/getAll.js
@@ -1,10 +1,15 @@
 module.exports = async (req, res) => {
     const handles = { code: 200, status: "OK", fail: [], data: [] };
+    const reqKeyword = req.body.keyword;
+    console.log(reqKeyword)
     try {
         if (handles.fail.length <= 0) {
+            let condition = [];
+            if (reqKeyword) { condition.push({ tracking: reqKeyword }); }
             let selParcels = await req.models.parcels.findAll(
                 {
                     attributes: ["parcels_id","tracking", "receiver_to", "description","created_at"],
+                    where: condition
                 }
             );
             if (!selParcels) { handles.code = 404; handles.status = "Not Found"; }
