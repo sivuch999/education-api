4584fdf3ce42e1146eff2bbfc7699ffad9e983cf
sivuch999 <slowsoup@hotmail.com>
2020-11-23 18:18:24 +0700

    add basic route

--- functions/app/routes/index.js
+++ functions/app/routes/index.js
@@ -2,5 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/parcels", require("./parcels"));
+router.post("/basic", (req, res) => {
+    const handles = { code: 200, status: "OK", fail: [], data: [] };
+    handles.code = 200; handles.status = "OK"; handles.data = [{ name: "soup", age: 25 },{ name: "max", age: 24 }]
+    res.json(handles);
+});
 
 module.exports = router;
